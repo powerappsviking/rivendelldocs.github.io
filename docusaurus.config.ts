643062a13818ec0368b4bcff0d629a4e03bb3939
powerappsviking <125479015+powerappsviking@users.noreply.github.com>
2024-08-17 20:01:00 +0200

    trailingslash update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,16 +8,17 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://powerappsviking.github.io/',
+  url: 'https://powerappsviking.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/rivendelldocs.github.io/',
+  baseUrl: '/rivendelldocs.github.io',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Powerappsviking', // Usually your GitHub org/user name.
   projectName: 'RivendellDocs.github.io', // Usually your repo name.
   deploymentBranch: 'rivendelldocs.github.io',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
