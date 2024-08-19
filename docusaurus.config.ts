7043c6b814004d93313a602aa3e338603932bd18
powerappsviking <125479015+powerappsviking@users.noreply.github.com>
2024-08-19 16:23:56 +0200

    url update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -87,7 +87,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/powerappsviking/rivendelldocs.github.io',
           label: 'GitHub',
           position: 'right',
         },
@@ -131,7 +131,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/powerappsviking/rivendelldocs.github.io',
             },
           ],
         },
